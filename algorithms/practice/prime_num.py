953482c94a4cc924b7114dfbab98f04affcb87b8
arshgill1 <68485728+arshgill1@users.noreply.github.com>
2021-07-14 18:25:48 -0400

    more prime variations

--- algorithms/practice/prime_num.py
+++ algorithms/practice/prime_num.py
@@ -22,3 +22,15 @@ isprime(arr)
 
 x=47
 primenum(x)
+
+# program to print prime numbers between 100 and 200
+
+n=int(input("enter a number between 100 and 200: "))
+while n>99 and n<201:
+    for i in range(2,n):
+        if n%i==0:
+            print("not prime")
+            break
+    else:
+        print(n," prime")
+    break
